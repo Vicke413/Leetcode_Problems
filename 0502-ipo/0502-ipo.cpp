4b86be8ae6e2d3e0cd13d720cdcdb2073bd40b07
Vicke413 <127419486+Vicke413@users.noreply.github.com>
2024-06-15 15:48:56 +0530

    Time: 129 ms (88.94%), Space: 85.5 MB (63.02%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        
+
+        priority_queue<int>pq;
+        int n=profits.size();
+vector<pair<int,int>>vp;
+
+for(int i=0;i<n;i++)
+{
+    vp.push_back({capital[i],profits[i]});
+}
+
+
+sort(vp.begin(),vp.end());
+
+int start=0;
+int total_profit=w;
+for(int i=0;i<k;i++)
+{
+
+
+while(start<n && vp[start].first<= total_profit)
+{
+    pq.push(vp[start].second);
+    start++;
+}
+
+if(pq.size()==0)
+break;
+total_profit+=pq.top();
+pq.pop();
+
+}
+
+return total_profit;
+
+    }
+};
