620cc33266286e5380f7e02bd46d8285dacd61c2
Vicke413 <127419486+Vicke413@users.noreply.github.com>
2024-06-24 19:52:30 +0530

    Time: 5 ms (25.86%), Space: 10.2 MB (87.07%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -3,37 +3,31 @@ public:
     vector<int> plusOne(vector<int>& digits) 
     {
         int n=digits.size();
-        vector<int>ans;
-    
-
-int c=0;
-int num;
+       
     for(int i=n-1;i>=0;i--)
     {
-        if(i==n-1)
-        num=digits[i]+c+1;
-        else
-        num=digits[i]+c;
 
-
-        if((num)>=10)
+        // 999
+        // 1000
+        if(digits[i]==9)
         {
-             ans.push_back((num)%10);
-            c=1;
+            digits[i]=0;
         }
         else
         {
-            ans.push_back(num%10);
-            c=0;
+            digits[i]=digits[i]+1;
+            return digits;
         }
+
+
+      
 
     }
+digits[0]=1;
 
-if(c)
-ans.push_back(1);
-reverse(ans.begin(),ans.end());
+digits.push_back(0);
 
-return ans;
+return digits;
        
         
     }
