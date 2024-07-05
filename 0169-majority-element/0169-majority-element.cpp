f56e647d01908e181eb86a1678daaa23897ea154
Vicke413 <127419486+Vicke413@users.noreply.github.com>
2024-07-05 17:51:00 +0530

    Time: 18 ms (54.04%), Space: 26.6 MB (70.35%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       
+
+       int can=0;
+
+int c=0;
+       for(int i=0;i<nums.size();i++)
+       {
+        if(c==0)
+        {
+            can=nums[i];
+        }
+
+if(can==nums[i])
+c++;
+else
+c--;
+
+
+       }
+       return can;
+    }
+};
