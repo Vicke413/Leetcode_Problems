be2292583ad1216e2e3c7a82ae409ebc3f51e328
Vicke413 <127419486+Vicke413@users.noreply.github.com>
2024-07-06 22:50:42 +0530

    Time: 120 ms (25%), Space: 76 MB (58.33%) - LeetHub

--- 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.cpp
+++ 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.cpp
@@ -6,15 +6,25 @@ int n=es.size();
 sort(es.begin(),es.end());
 
 long long points=0;
-bool ok=true;
 
-if(cur<es[0])
+
+for(int i=0;i<n;i++)
+{
+    if(cur>= es[i]  )
+    {
+        points++;
+        cur-=es[i];
+       
+        break;
+    }
+}
+
+if(points==0)
 return 0;
 
 long long ac=0;
 
 ac=accumulate(es.begin(),es.end(),0LL);
-ac-=es[0];
 ac+=cur;
 
 return ac/es[0];
