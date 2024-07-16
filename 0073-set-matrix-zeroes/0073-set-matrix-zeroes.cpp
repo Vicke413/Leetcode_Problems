c9c868dcbc6f62a9d5f7429d6332cab050ab97ad
Vicke413 <127419486+Vicke413@users.noreply.github.com>
2024-07-16 11:48:13 +0530

    Time: 8 ms (76.79%), Space: 16.8 MB (22.05%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -25,32 +25,61 @@ void dfs(int i,int j, vector<vector<int>>& matrix , int n,int m)
         int n=matrix.size();
 
         int m=matrix[0].size();
-
+bool f1=false;
+bool f2=false;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
-            {
+            if(matrix[i][0]==0)
+            f1=true;
 
-                if(matrix[i][j]==0)
-                {
-                    dfs(i,j,matrix,n,m);
-                }
-            }
         }
-
-          for(int i=0;i<n;i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
-            {
-
-                if(matrix[i][j]==-99999)
-                {
-                    matrix[i][j]=0;
-                }
-            }
+            if(matrix[0][i]==0)
+            f2=true;
         }
 
 
-        
+for(int i=1;i<n;i++)
+{
+    for(int j=1;j<m;j++)
+    {
+        if(matrix[i][j]==0)
+        {
+            matrix[0][j]=0;
+            matrix[i][0]=0;
+        }
+    }
+}
+
+
+for(int i=1;i<n;i++)
+{
+    for(int j=1;j<m;j++)
+    {
+        if(matrix[0][j]==0 || matrix[i][0]==0)
+        {
+          matrix[i][j]=0;
+        }
+    }
+}
+if(f1)
+{
+    for(int i=0;i<n;i++)
+    {
+        matrix[i][0]=0;
+    }
+}
+
+      if(f2)
+{
+    for(int i=0;i<m;i++)
+    {
+        matrix[0][i]=0;
+    }
+}  
+
+// TC=O(N*M)
+// SC=O(1)
     }
 };
