a07a7e0435a47b29325d0e1493fc46b3736c7094
Vicke413 <127419486+Vicke413@users.noreply.github.com>
2024-08-29 00:16:38 +0530

    Time: 226 ms (47.39%), Space: 100.2 MB (49.04%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.cpp
+++ 2035-count-sub-islands/2035-count-sub-islands.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int x, int y, int &res) {
-        int m = grid1.size(), n = grid1[0].size();
-        if(x < 0 || y < 0 || x >= m || y >= n) return;
-        if(grid1[x][y] == 0 && grid2[x][y] == 1) {
-            res = false;
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+
+    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int n, int m, vector<vector<int>>& vis, int i, int j, bool& isSubIsland) {
+        vis[i][j] = 1;
+
+        // If this cell is 1 in grid2 but 0 in grid1, it's not a sub-island
+        if (grid1[i][j] == 0) {
+            isSubIsland = false;
         }
-        if(grid2[x][y] == 0) return;
-        grid2[x][y] = 0;
-        dfs(grid1, grid2, x + 1, y, res);
-        dfs(grid1, grid2, x, y + 1, res);
-        dfs(grid1, grid2, x - 1, y, res);
-        dfs(grid1, grid2, x, y - 1, res);
+
+        for (int k = 0; k < 4; k++) {
+            int nr = dx[k] + i;
+            int nc = dy[k] + j;
+            if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid2[nr][nc] == 1 && vis[nr][nc] == 0) {
+                dfs(grid1, grid2, n, m, vis, nr, nc, isSubIsland);
+            }
+        }
     }
+
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int m = grid1.size(), n = grid1[0].size();
-        int ans = 0;
-        for(int i = 0; i < m ; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid2[i][j] == 1) {
-                    int res = true;
-                    dfs(grid1, grid2, i, j, res);
-                    ans += res;
+        int n = grid1.size();
+        int m = grid1[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid2[i][j] == 1 && vis[i][j] == 0) {
+                    bool isSubIsland = true;
+                    dfs(grid1, grid2, n, m, vis, i, j, isSubIsland);
+                    if (isSubIsland) {
+                        count++;
+                    }
                 }
             }
         }
-        return ans;
+        return count;
     }
 };
