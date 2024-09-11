0b487814c0ae4ad917ac2c2629cb4e8a519111cb
Vicke413 <127419486+Vicke413@users.noreply.github.com>
2024-09-11 18:38:17 +0530

    Time: 0 ms (100%), Space: 7.5 MB (93.51%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -4,11 +4,16 @@ public:
 
 
     int xo=start ^ goal;
+    //we gonna use it later
     int c=0;
 
     for(int i=0;i<31;i++)
+    // interate all 32 bits
     {
         if(xo & (1<<i))
+        // checking how many bits are differs, 
+        //so xor is 1 , we increment count
+
         c++;
     }
 
